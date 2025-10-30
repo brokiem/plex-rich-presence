d2c6515e42454f257f18fb78a86ba1ec98771606
Ombrelin <arsene@lapostolet.fr>
2025-10-31 00:03:54 +0100

    Adds new python-powered script and workflow to issue an app release

---
+++ ci/release-tag.py
@@ -0,0 +1,59 @@
+import sys
+from git import Repo, TagReference
+from github import Github
+from github import Auth
+
+
+def update_app_version(repo: Repo, version: str):
+    file_name = "src/PlexRichPresence.UI.Avalonia/PlexRichPresence.UI.Avalonia.csproj"
+    lines = open(file_name, "r").readlines()
+    lines[6] = "        <Version>" + version + "</Version>\n"
+    out = open(file_name, "w")
+    out.writelines(lines)
+    out.close()
+
+    repo.index.add(file_name)
+    repo.index.commit("Update app version : " + version)
+    repo.remotes.origin.push()
+
+
+def get_next_version(current_version: str) -> str:
+    version_parts = current_version.split(".")
+
+    if version_kind == "major":
+        version_parts[0] = str(int(version_parts[0]) + 1)
+    elif version_kind == "minor":
+        version_parts[1] = str(int(version_parts[1]) + 1)
+    elif version_kind == "patch":
+        version_parts[2] = str(int(version_parts[2]) + 1)
+    return ".".join(version_parts)
+
+
+def get_current_version(github_token: str) -> str:
+    auth = Auth.Token(github_token)
+    github = Github(auth=auth)
+    repo = github.get_repo("ombrelin/plex-rich-presence")
+    return repo.get_latest_release().name
+
+
+github_token: str = sys.argv[1]
+version_kind: str = sys.argv[2]
+
+print("Issuing release...")
+print("Version kind : " + version_kind)
+
+current_version: str = get_current_version(github_token)
+print("Current version : " + current_version)
+new_version: str = get_next_version(current_version)
+print("Next version : " + new_version)
+
+with open("version.txt", "a") as f:
+    f.write(new_version)
+
+
+repo: Repo = Repo(".")
+update_app_version(repo, new_version)
+new_tag: TagReference = repo.create_tag(
+    new_version, message='Version "{0}"'.format(new_version)
+)
+repo.remotes.origin.push(new_tag.name)

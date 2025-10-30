e2ad56949c47e5771651dfb6851c1ada2adbac7a
Ombrelin <arsene@lapostolet.fr>
2025-10-30 21:43:53 +0100

    Fixes release script

--- ci/release-tag.py
+++ ci/release-tag.py
@@ -1,24 +1,41 @@
 import sys
-from git import Repo
+from git import Repo, TagReference
+from github import Github
+from github import Auth
 
-current_tag = sys.argv[1]
-version_kind = sys.argv[2]
+
+def get_next_version(current_version: str) -> str:
+    version_parts = current_version.split(".")
+
+    if version_kind == "major":
+        version_parts[0] = str(int(version_parts[0]) + 1)
+    elif version_kind == "minor":
+        version_parts[1] = str(int(version_parts[1]) + 1)
+    elif version_kind == "patch":
+        version_parts[2] = str(int(version_parts[2]) + 1)
+    return ".".join(version_parts)
+
+
+def get_current_version(github_token: str) -> str:
+    auth = Auth.Token(github_token)
+    github = Github(auth=auth)
+    repo = github.get_repo("ombrelin/plex-rich-presence")
+    return repo.get_latest_release().name
+
+
+github_token: str = sys.argv[1]
+version_kind: str = sys.argv[2]
 
 print("Issuing release...")
-print("Current tag : " + current_tag)
-print("Version tag : " + version_kind)
+print("Version kind : " + version_kind)
 
-version_parts = current_tag.split('.')
+current_version: str = get_current_version(github_token)
+print("Current version : " + current_version)
+new_version: str = get_next_version(current_version)
+print("Next version : " + new_version)
 
-if version_kind == "major":
-    version_parts[0] = str(int(version_parts[0]) + 1)
-elif version_kind == "minor":
-    version_parts[1] = str(int(version_parts[1]) + 1)
-elif version_kind == "patch":
-    version_parts[2]= str(int(version_parts[2]) + 1)
-    
-new_version = ".".join(version_parts)
-print("Next version tag : " + new_version)
-repo = Repo(".")
-new_tag = repo.create_tag(new_version, message='Version "{0}"'.format(new_version))
-repo.remotes.origin.push(new_tag)
+repo: Repo = Repo(".")
+new_tag: TagReference = repo.create_tag(
+    new_version, message='Version "{0}"'.format(new_version)
+)
+_ = repo.remotes.origin.push(new_tag.name)
